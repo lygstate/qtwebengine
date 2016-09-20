fe73aba9a61279110682e08c2384082601342ed2
Szabolcs David <davidsz@inf.u-szeged.hu>
2016-09-20 03:51:55 -0700

    Remove experimental Quick API
    QtWebEngine.experimental import is not working. It is better to
    get rid of that.
    It also removes some QNX-specific code.
    
    Change-Id: If80c1290b4d5f5e4d436c49bd7d7a6ea882ee6db
    Reviewed-by: Peter Varga <pvarga@inf.u-szeged.hu>

--- src/webengine/api/qquickwebengineview_p.h
+++ src/webengine/api/qquickwebengineview_p.h
@@ -63,7 +63,6 @@ class QQuickWebEngineNavigationRequest;
 class QQuickWebEngineNewViewRequest;
 class QQuickWebEngineProfile;
 class QQuickWebEngineSettings;
-class QQuickWebEngineViewExperimental;
 class QQuickWebEngineViewPrivate;
 
 #ifdef ENABLE_QML_TESTSUPPORT_API
@@ -134,13 +133,11 @@ public:
     QColor backgroundColor() const;
     void setBackgroundColor(const QColor &color);
 
-    QQuickWebEngineViewExperimental *experimental() const;
-
     // must match WebContentsAdapterClient::NavigationRequestAction
     enum NavigationRequestAction {
         AcceptRequest,
         // Make room in the valid range of the enum so
-        // we can expose extra actions in experimental.
+        // we can expose extra actions.
         IgnoreRequest = 0xFF
     };
     Q_ENUM(NavigationRequestAction)
@@ -328,8 +325,6 @@ private:
     Q_DECLARE_PRIVATE(QQuickWebEngineView)
     QScopedPointer<QQuickWebEngineViewPrivate> d_ptr;
 
-    friend class QQuickWebEngineViewExperimental;
-    friend class QQuickWebEngineViewExperimentalExtension;
     friend class QQuickWebEngineNewViewRequest;
 #ifndef QT_NO_ACCESSIBILITY
     friend class QQuickWebEngineViewAccessible;
